0f3979ff2a3c0dd8c4338d3a008b28ca431806a1
DongJoon <dkim00610@gmail.com>
2024-05-12 21:41:00 +0900

    second commit

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/dog', (req, res) => {
+  res.send('Dog')
+})
+
+app.get('/cat', (req, res) => {
+  res.send('Cat')
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
